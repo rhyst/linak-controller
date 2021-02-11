a7794fdbe19471b892341cd746696c16f5c6f697
Rhys Tyers <rhystyers1@gmail.com>
2021-02-11 17:12:42 +0000

    Bump version to 1.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [1.0.5] - 2021-02-11
+
+### Changed
+
+- Fix `--scan-timeout` flag.
+
 ## [1.0.4] - 2021-02-11
 
 ### Changed
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md')) as f:
   
 setup( 
         name ='idasen-controller', 
-        version ='1.0.4', 
+        version ='1.0.5', 
         author ='Rhys Tyers', 
         author_email ='', 
         url ='https://github.com/rhyst/idasen-controller', 
@@ -34,4 +34,5 @@ setup(
         zip_safe = False,
         include_package_data = True,
         package_data={'': ['example/*']},
+        python_requires='>=3.7.3'
 ) 
