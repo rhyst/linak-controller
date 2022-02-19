34cf116c77a2c5e226f557c434c69bb5f7f6d9c1
Shaw Young <Shawson@users.noreply.github.com>
2022-02-19 20:56:16 +0000

    removing get_height in favour of allowing empty command to be passes

--- idasen_controller/main.py
+++ idasen_controller/main.py
@@ -76,7 +76,6 @@ config = {
     "movement_timeout": 30,
     "sit": False,
     "stand": False,
-    "get_height": False,
     "monitor": False,
     "move_to": None,
     "move_to_raw": None,
@@ -498,7 +497,7 @@ async def run_forwarded_command(client, config, reader, writer):
 async def forward_command(config):
     """Send commands to the tcp server"""
 
-    allowed_keys = ["sit", "stand", "move_to", "move_to_raw", "get_height"]
+    allowed_keys = ["sit", "stand", "move_to", "move_to_raw", ""]
     forwarded_config = {key: config[key] for key in allowed_keys if key in config}
     reader, writer = await asyncio.open_connection(
         config["server_address"], config["server_port"]
