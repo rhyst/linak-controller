caaf2553480b02bd9549e656b88ce3b1bab5ace0
Toms Binde <toms.binde@gmail.com>
2023-11-03 14:33:26 +0200

    This resolves rare desk stuttering during movement

--- linak_controller/desk.py
+++ linak_controller/desk.py
@@ -63,7 +63,7 @@ class Desk:
 
         while True:
             await ReferenceInputService.ONE.write(client, data)
-            await asyncio.sleep(0.4)
+            await asyncio.sleep(0.3)
             height, speed = await ReferenceOutputService.get_height_speed(client)
             if speed.value == 0:
                 break
--- linak_controller/gatt.py
+++ linak_controller/gatt.py
@@ -93,7 +93,7 @@ class ReferenceInputService(Service):
 
         while True:
             await cls.ONE.write(client, data)
-            await asyncio.sleep(0.4)
+            await asyncio.sleep(0.3)
             height, speed = await ReferenceOutputService.get_height_speed(client)
             if speed.value == 0:
                 break
